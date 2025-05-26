8b09a30f16db2df9ddcff39007704803255484e6
Meksak Chilingaryan <mexakchilingaryan3@gmail.com>
2025-05-27 00:46:33 +0400

    Initial backend commit

---
+++ server.js
@@ -0,0 +1,35 @@
+// server.js
+
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { checkTelegramAuth } = require('./utils');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from My Coin Backend 🚀');
+});
+
+app.post('/auth/telegram', (req, res) => {
+  const { id, username, first_name, last_name, hash } = req.body;
+  const botToken = process.env.TELEGRAM_BOT_TOKEN;
+
+  const isValid = checkTelegramAuth(req.body, botToken);
+
+  if (isValid) {
+    res.json({
+      success: true,
+      message: `Բարև, ${first_name || username || id}!`,
+      user: { id, username, first_name, last_name },
+    });
+  } else {
+    res.json({ success: false, message: 'Invalid Telegram login' });
+  }
+});
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
---
+++ utils.js
@@ -0,0 +1,18 @@
+// utils.js
+
+
+const crypto = require('crypto');
+
+function checkTelegramAuth(data, botToken) {
+  const { hash, ...rest } = data;
+  const secret = crypto.createHash('sha256').update(botToken).digest();
+  const checkString = Object.keys(rest)
+    .sort()
+    .map(key => `${key}=${rest[key]}`)
+    .join('\n');
+
+  const hmac = crypto.createHmac('sha256', secret).update(checkString).digest('hex');
+  return hmac === hash;
+}
+
+module.exports = { checkTelegramAuth };
