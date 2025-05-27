cb07f1593ea612be41b19692d6eeb6d617817884
Meksak Chilingaryan <mexakchilingaryan3@gmail.com>
2025-05-28 01:36:55 +0400

    chore: Update default PORT value for local development

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { checkTelegramAuth } = require('./utils'); // ✅ ուղին այսպես թող, եթե utils.js ու index.js միևնույն մակարդակում են
+const { checkTelegramAuth } = require('./utils'); 
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
     res.send('Hello from My Coin Backend 🚀');
